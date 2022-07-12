c1d4f22e4099d7afe78433b937a522d51e4f16d1
PRANESH KUMAR SHUKLA <99534741+pranu1993@users.noreply.github.com>
2022-07-12 13:50:19 +0530

    Add files via upload

---
+++ project 3.js
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Arrays{
+
+    public static void main(String args[]) {
+
+    int[] marks = new int[3];
+    marks[0]= 97;
+    marks[1]= 91;
+    marks[2]=96;
+
+    system.out.println(marks[0]);
+    system.out.println(marks[1]);
+    system.out.println(marks[2]);
+
+    }
+}
